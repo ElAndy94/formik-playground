1ca3ee49a83ff44680f5f3d6bc65cf1a8e637703
Andrew <andrewpeliza@hotmail.com>
2020-03-02 14:44:11 +0000

    Having a little play around with material UI and Formik forms.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import { TextField, Button } from '@material-ui/core';
+import { Formik } from 'formik';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='form'>
+      <Formik
+        initialValues={{ firstName: '' }}
+        onSubmit={data => {
+          console.log('submit: ', data);
+        }}
+      >
+        {({ values, handleChange, handleBlur, handleSubmit }) => (
+          <form onSubmit={handleSubmit}>
+            <TextField
+              name='firstName'
+              value={values.firstName}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+            <div>
+              <Button type='submit'>Submit</Button>
+            </div>
+            <pre>{JSON.stringify(values, null, 2)}</pre>
+          </form>
+        )}
+      </Formik>
     </div>
   );
-}
+};
 
 export default App;
