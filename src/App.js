af5bee0f8539cc89130d04438a348057b46562a8
Andrew <andrewpeliza@hotmail.com>
2020-03-02 15:15:41 +0000

    Checkbox, Radio and custom fields, using Formik useField hook.

--- src/App.js
+++ src/App.js
@@ -1,43 +1,92 @@
 import React from 'react';
 
 import './App.css';
-import { TextField, Button } from '@material-ui/core';
-import { Formik } from 'formik';
+import {
+  TextField,
+  Button,
+  Checkbox,
+  Radio,
+  FormControlLabel
+} from '@material-ui/core';
+import { Formik, Field, Form, useField } from 'formik';
+
+const MyRadio = ({ label, ...props }) => {
+  const [field] = useField(props);
+  return <FormControlLabel {...field} control={<Radio />} label={label} />;
+};
 
 const App = () => {
   return (
     <div className='form'>
       <Formik
-        initialValues={{ firstName: '', lastname: '' }}
+        initialValues={{
+          firstName: '',
+          lastName: '',
+          isTall: false,
+          cookies: [],
+          yogurt: ''
+        }}
         onSubmit={(data, { setSubmitting }) => {
-          setSubmitting(true);
           // make async call
+          setSubmitting(true);
           console.log('submit: ', data);
-
           setSubmitting(false);
         }}
       >
-        {({ values, isSubmitting, handleChange, handleBlur, handleSubmit }) => (
-          <form onSubmit={handleSubmit}>
-            <TextField
-              name='firstName'
-              value={values.firstName}
-              onChange={handleChange}
-              onBlur={handleBlur}
+        {({ values, isSubmitting }) => (
+          <Form>
+            <div>
+              <Field
+                placeholder='first name'
+                name='firstName'
+                type='input'
+                as={TextField}
+              />
+            </div>
+            <div>
+              <Field
+                placeholder='last name'
+                name='lastName'
+                type='input'
+                as={TextField}
+              />
+            </div>
+            <div>
+              <Field name='isTall' type='checkbox' as={Checkbox} />
+            </div>
+            <div>Cookies: </div>
+            <Field
+              name='cookies'
+              type='checkbox'
+              vlue='chocolate chip'
+              as={Checkbox}
             />
-            <TextField
-              name='lastName'
-              value={values.lastName}
-              onChange={handleChange}
-              onBlur={handleBlur}
+            <Field
+              name='cookies'
+              type='checkbox'
+              vlue='snickerdoodle'
+              as={Checkbox}
             />
+            <Field name='cookies' type='checkbox' vlue='sugar' as={Checkbox} />
+            <div>Yogurt</div>
+            <MyRadio name='yogurt' type='radio' value='peach' label='peach' />
+            <MyRadio
+              name='yogurt'
+              type='radio'
+              value='blueberry'
+              label='blueberry'
+            />
+            <MyRadio name='yogurt' type='radio' value='apple' label='apple' />
+            {/* <Field name='yogurt' type='radio' value='peach' as={Radio} />
+            <Field name='yogurt' type='radio' value='blueberry' as={Radio} />
+            <Field name='yogurt' type='radio' value='apple' as={Radio} /> */}
             <div>
               <Button disabled={isSubmitting} type='submit'>
                 Submit
               </Button>
             </div>
             <pre>{JSON.stringify(values, null, 2)}</pre>
-          </form>
+          </Form>
         )}
       </Formik>
     </div>
